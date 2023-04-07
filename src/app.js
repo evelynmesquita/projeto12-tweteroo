5adae877ffc7ee5938550e98f9cd2a9077cd3d68
Evelyn <evelynbandeiramesquita@gmail.com>
2023-04-07 12:49:56 -0300

    add base code

--- src/app.js
+++ src/app.js
@@ -6,3 +6,25 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
+
+const user = [];
+const tweets = [];
+
+
+app.post("/sing-up", (req, res) => {
+    res.send('testando')
+})
+
+app.post("/tweets", (req, res) => {
+    res.send('testando')
+})
+
+app.get("/tweets", (req, res) => {
+    res.send("testando")
+})
+
+
+
+app.listen(5000, () => {
+	console.log("Server on")
+})
